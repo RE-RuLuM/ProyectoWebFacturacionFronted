8d1219161f55a5455904ec4ab16c1a72487f287a
Edgar Moreano Rodriguez <edgar12moreano@hotmail.com>
2024-03-23 19:34:43 -0500

    fix: correccion de muestra de mensaje

--- src/infraestructure/axios.ts
+++ src/infraestructure/axios.ts
@@ -7,7 +7,7 @@ instance.interceptors.response.use(response => response, (error) => {
   const response = error.response
 
   if (response?.status === 400 || response?.status === 401) {
-    toast.error(error.response.data.title, { duration: 5000 });
+    toast.error(error.response.data.title ?? error.response.data.msg, { duration: 5000 });
   }
 
   return Promise.reject(error)
